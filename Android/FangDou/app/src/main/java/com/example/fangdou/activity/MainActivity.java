a478e469df5796ba4da8d34b696913bdebb0a88a
KangYh <34295007+0-0MrLonely@users.noreply.github.com>
2019-03-17 02:11:15 +0800

    1

--- Android/FangDou/app/src/main/java/com/example/fangdou/activity/MainActivity.java
+++ Android/FangDou/app/src/main/java/com/example/fangdou/activity/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.fangdou.activity;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -13,13 +16,15 @@ import android.widget.Toast;
 
 import com.example.fangdou.R;
 import com.example.fangdou.adapter.VideoAdapter;
+import com.example.fangdou.fragment.SquareFragment;
+import com.example.fangdou.fragment.navigationFragment;
 
 import java.util.ArrayList;
 
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
 import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
 
-public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener
+public class MainActivity extends AppCompatActivity
 {
     private String videoUrl = "http://gslb.miaopai.com/stream/ed5HCfnhovu3tyIQAiv60Q__.mp4";
     private ListView listView;
@@ -34,124 +39,21 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_square);
-        listView = (ListView) findViewById(R.id.listview);
-        new NavigationBar(1);
-
-        radioGroup = (RadioGroup) findViewById(R.id.tabs_rg);
-        Log.d("haha", "123");
-        radioGroup.setOnCheckedChangeListener(this);
-
-        initDatas();
-        initListener();
+        setContentView(R.layout.activity_main);
+        addFragment(new SquareFragment(),R.id.square_fragment);
+        addFragment(new navigationFragment(),R.id.navigation_fragment);
     }
 
-
-    private void initDatas() {
-        datas = new ArrayList<String>();
-        for (int i = 0; i < 20; i++) {
-            datas.add(videoUrl);
-        }
-        adapter = new VideoAdapter(MainActivity.this, datas, R.layout.item_video);
-        System.out.println(listView);
-        listView.setAdapter(adapter);
-    }
-
-    /**
-     * 滑动监听
-     */
-    private void initListener() {
-        onScrollListener = new AbsListView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-                switch (scrollState) {
-                    case AbsListView.OnScrollListener.SCROLL_STATE_FLING:
-                        break;
-
-                    case AbsListView.OnScrollListener.SCROLL_STATE_IDLE:
-                        //滑动停止自动播放视频
-                        //autoPlayVideo(view);
-                        break;
-
-                    case AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
-                        break;
-                }
-            }
-
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                if (firstVisible == firstVisibleItem) {
-                    return;
-                }
-
-                firstVisible = firstVisibleItem;
-                visibleCount = visibleItemCount;
-            }
-        };
-
-        listView.setOnScrollListener(onScrollListener);
-    }
-
-    /**
-     * 滑动停止自动播放视频
-     */
-    private void autoPlayVideo(AbsListView view) {
-
-        for (int i = 0; i < visibleCount; i++) {
-            if (view != null && view.getChildAt(i) != null && view.getChildAt(i).findViewById(R.id.player_list_video) != null) {
-                currPlayer = (JCVideoPlayerStandard) view.getChildAt(i).findViewById(R.id.player_list_video);
-                Rect rect = new Rect();
-                //获取当前view 的 位置
-                currPlayer.getLocalVisibleRect(rect);
-                int videoheight = currPlayer.getHeight();
-                if (rect.top == 0 && rect.bottom == videoheight) {
-                    if (currPlayer.currentState == JCVideoPlayer.CURRENT_STATE_NORMAL
-                            || currPlayer.currentState == JCVideoPlayer.CURRENT_STATE_ERROR) {
-                        currPlayer.startButton.performClick();
-                    }
-                    return;
-                }
-            }
-        }
-        //释放其他视频资源
-        JCVideoPlayer.releaseAllVideos();
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (JCVideoPlayer.backPress()) {
-            return;
-        }
-        super.onBackPressed();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        JCVideoPlayer.releaseAllVideos();
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId)
+    private void addFragment(Fragment fragment,int id)
     {
-        switch (checkedId)
-        {
-            case R.id.square:
-                Intent intent = new Intent("android.intent.action.MAIN");
-                intent.addCategory("android.intent.category.LAUNCHER");
-                intent.addCategory("android.intent.category.start_square");
-                startActivity(intent);
-                Toast.makeText(this, "123", Toast.LENGTH_LONG);
-                break;
-            case R.id.knowledge:
-                break;
-            case R.id.start_record:
-                break;
-            default:
-                break;
 
-        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(id, fragment);
+
+        fragmentTransaction.commit();
 
     }
+
 }
---
+++ Android/FangDou/app/src/main/java/com/example/fangdou/fragment/SquareFragment.java
@@ -0,0 +1,165 @@
+package com.example.fangdou.fragment;
+
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.fangdou.R;
+import com.example.fangdou.activity.MainActivity;
+import com.example.fangdou.activity.NavigationBar;
+import com.example.fangdou.adapter.VideoAdapter;
+
+import java.util.ArrayList;
+
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayer;
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
+
+public class SquareFragment extends Fragment
+{
+    private ListView listView;
+    private RadioGroup radioGroup;
+    private String videoUrl = "http://gslb.miaopai.com/stream/ed5HCfnhovu3tyIQAiv60Q__.mp4";
+    private ArrayList<String> datas;
+    private JCVideoPlayerStandard currPlayer;
+    private VideoAdapter adapter;
+    private AbsListView.OnScrollListener onScrollListener;
+    private int firstVisible;//当前第一个可见的item
+    private int visibleCount;//当前可见的item个数
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        //若引用子布局，则会报错llegalStateException: The specified child already has a parent
+        //所以要将父子布局分离，使用removeView.
+        //又因为onCreateView会多次调用，所以view可以重复利用。
+        if (view != null)
+        {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+            {
+                parent.removeView(view);
+            }
+
+        } else
+        {
+            view = inflater.inflate(R.layout.activity_square,null);
+            listView = (ListView) view.findViewById(R.id.listview);
+
+        }
+        initDatas();
+        initListener();
+        return view;
+
+    }
+
+
+
+    private void initDatas()
+    {
+        datas = new ArrayList<String>();
+        for (int i = 0; i < 20; i++)
+        {
+            datas.add(videoUrl);
+        }
+        adapter = new VideoAdapter(view.getContext(), datas, R.layout.item_video);
+        listView.setAdapter(adapter);
+    }
+
+    /**
+     * 滑动监听
+     */
+    private void initListener()
+    {
+        onScrollListener = new AbsListView.OnScrollListener()
+        {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState)
+            {
+
+                switch (scrollState)
+                {
+                    case AbsListView.OnScrollListener.SCROLL_STATE_FLING:
+                        break;
+
+                    case AbsListView.OnScrollListener.SCROLL_STATE_IDLE:
+                        //滑动停止自动播放视频
+                        //autoPlayVideo(view);
+                        break;
+
+                    case AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
+                        break;
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
+            {
+                if (firstVisible == firstVisibleItem)
+                {
+                    return;
+                }
+
+                firstVisible = firstVisibleItem;
+                visibleCount = visibleItemCount;
+            }
+        };
+
+        listView.setOnScrollListener(onScrollListener);
+    }
+
+    /**
+     * 滑动停止自动播放视频
+     */
+    private void autoPlayVideo(AbsListView view)
+    {
+
+        for (int i = 0; i < visibleCount; i++)
+        {
+            if (view != null && view.getChildAt(i) != null && view.getChildAt(i).findViewById(R.id.player_list_video) != null)
+            {
+                currPlayer = (JCVideoPlayerStandard) view.getChildAt(i).findViewById(R.id.player_list_video);
+                Rect rect = new Rect();
+                //获取当前view 的 位置
+                currPlayer.getLocalVisibleRect(rect);
+                int videoheight = currPlayer.getHeight();
+                if (rect.top == 0 && rect.bottom == videoheight)
+                {
+                    if (currPlayer.currentState == JCVideoPlayer.CURRENT_STATE_NORMAL
+                            || currPlayer.currentState == JCVideoPlayer.CURRENT_STATE_ERROR)
+                    {
+                        currPlayer.startButton.performClick();
+                    }
+                    return;
+                }
+            }
+        }
+        //释放其他视频资源
+        JCVideoPlayer.releaseAllVideos();
+    }
+
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        JCVideoPlayer.releaseAllVideos();
+    }
+
+
+}
+
+
