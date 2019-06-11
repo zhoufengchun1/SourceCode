b291cab8cb3aaea3707fb1f49f882ab82dfbf473
KangYh <34295007+0-0MrLonely@users.noreply.github.com>
2019-06-11 18:45:02 +0800

    1

--- C#/OCS/添加好友.cs
+++ C#/OCS/添加好友.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using PMS;
@@ -10,8 +11,8 @@ namespace OCS
         private MySqlConnection mySqlConnection = SetConnection.mySqlConnection;
         private string Name, attr;
         private MySqlDbType mySqlDbType;
-        
-       
+
+
         public 添加好友()
         {
             InitializeComponent();
@@ -43,32 +44,37 @@ namespace OCS
             this.userTableAdapter.Fill(this.oCSDataSet.user);
             // TODO: 这行代码将数据加载到表“oCSDataSet.user”中。您可以根据需要移动或删除它。
             this.userTableAdapter.Fill(this.oCSDataSet.user);
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            listView1.Items.Clear();
+            if (textBox1.Visible&& textBox1.Text == "")
+            {
+                MessageBox.Show("请输入查询数据!", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             switch (comboBox1.Text)
             {
                 case "学号/工号":
                     Name = "userId";
-                    mySqlDbType=MySqlDbType.Int16;
+                    mySqlDbType = MySqlDbType.Int16;
                     break;
                 case "用户名":
                     Name = "userName";
-                    mySqlDbType=MySqlDbType.String;
+                    mySqlDbType = MySqlDbType.String;
                     break;
                 case "性别":
                     Name = "userSex";
-                    mySqlDbType=MySqlDbType.String;
+                    mySqlDbType = MySqlDbType.String;
                     break;
                 case "所在班级":
                     Name = "userClass";
-                    mySqlDbType=MySqlDbType.String;
+                    mySqlDbType = MySqlDbType.String;
                     break;
                 case "所在系":
                     Name = "userDepname";
-                    mySqlDbType=MySqlDbType.String;
+                    mySqlDbType = MySqlDbType.String;
                     break;
             }
 
@@ -82,22 +88,27 @@ namespace OCS
             }
 
             string cmd =
-                "select * from user where "+Name+"=@attr";
+                "select * from user where " + Name + "=@attr";
             MySqlCommand mySqlCommand = new MySqlCommand(cmd, mySqlConnection);
             mySqlCommand.Parameters.Add("@attr", mySqlDbType);
             mySqlCommand.Parameters["@attr"].Value = attr;
 
             MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
-            
             while (mySqlDataReader.Read())
             {
                 ListViewItem listViewItem = new ListViewItem();
-                listViewItem.Text = mySqlDataReader["userId"].ToString();
+//                listViewItem.Text = mySqlDataReader["userId"].ToString();
+                listViewItem.SubItems.Add(mySqlDataReader["userId"].ToString());
                 listViewItem.SubItems.Add(mySqlDataReader["userName"].ToString());
                 listViewItem.SubItems.Add(mySqlDataReader["userSex"].ToString());
                 listViewItem.SubItems.Add(mySqlDataReader["userClass"].ToString());
                 listViewItem.SubItems.Add(mySqlDataReader["userDepname"].ToString());
                 listView1.Items.Add(listViewItem);
+            }
+
+            if (listView1.Items.Count == 0)
+            {
+                MessageBox.Show("未查询到相关数据。");
             }
             mySqlDataReader.Close();
         }
